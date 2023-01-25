13d9f63870950d4791cdf0b4fbe9fb80e157bfbb
AngelinaEiras <angie.eiras@gmail.com>
2023-01-25 14:39:56 +0000

    parte 2

---
+++ kag/rascunho.py
@@ -0,0 +1,11 @@
+'''
+A alterar:
+
+    - Poucos descritores: gerar mais com o propythia
+    - Separar o x do y antes de fazer standardscaler e usar o y sem alterar os valores 
+    - Retirar valores de pH maiores que 14
+    - Fazer análise com base na literatura, exemplo, ponto em que a termoestabilidade representa a estabilidade etc
+    - Aumentar número de features, 
+    - não e preciso normalizar (podemos fazer scale e apenas no X), 
+    - machine learning com regressão (tm variável continua)
+'''
